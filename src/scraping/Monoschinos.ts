5bf7e178c0152fadeb1d04618723980025987962
Sebastián Afanador <sebastianafanador95@gmail.com>
2024-10-02 20:05:28 -0500

    Update Monoschinos.ts

--- src/scraping/Monoschinos.ts
+++ src/scraping/Monoschinos.ts
@@ -23,8 +23,7 @@ export class Monoschinos {
 
     try {
       browser = await puppeteer.launch({
-        executablePath:
-          process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser',
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome',
         headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
       });
